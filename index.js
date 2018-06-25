00da5923effcd6118350b0074a42cac72af32f2b
Anil Balram <adbalramd1@gmail.com>
2018-06-25 23:04:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,44 +1,37 @@
-var number1 = 5
+var number = 5
 number++
-return number1
 
 number--
-return number1
 
 number += 3
-return number1
 
 number -= 2
-return number1
 
 number *= 10
-return number1
 
 number /= 5
-return number1
 
-var number2 = 10
+var number = 10
 function add5()
 {
   number += 5
-  return number2
 }
 
 function divideBy3()
 {
   number /= 3
-  return number2
 }
 
+divideBy3()
+console.log(number)
 
-var Int
+add5()
+console.log(number)
+
 parseInt('2', 10)
-return Int
-parseInt('2.2222', 10)
-return Int
-parseInt('nonsense!', 10)
-return Int
 
-var Float
+parseInt('2.2222', 10)
+
+parseInt('nonsense!', 10)
+
 parseFloat('80.123999')
-return Float
