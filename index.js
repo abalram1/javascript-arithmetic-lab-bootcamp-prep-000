7fce8cd2e3e8bec495450f6f41a5d588456cf686
Anil Balram <adbalramd1@gmail.com>
2018-06-25 22:55:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+var number = 5
+number++
+return number
+number--
+return number
+
+number += 3
+return number
+
+number -= 2
+return number
+
+number *= 10
+return number
+
+number /= 5
+return number
+
+var number = 10
+function add5()
+{
+  number += 5
+}
+
+function divideBy3()
+{
+  number /= 3
+}
+
+return number
+
+var Int
+parseInt('2', 10)
+return Int
+parseInt('2.2222', 10)
+return Int
+parseInt('nonsense!', 10)
+return Int
+
+var Float
+parseFloat('80.123999')
+return Float
