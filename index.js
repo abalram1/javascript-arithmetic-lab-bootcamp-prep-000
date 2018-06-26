b1254dd6ac3b8b812d187d20ee951e3a2907b49a
Anil Balram <adbalramd1@gmail.com>
2018-06-26 20:25:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ var number = 10
 
 function add5(){
   number += 5
+  return number
 }
 
 function divideBy3(){
   number /= 3
   return number
-  
 }
 
 divideBy3(number)
 
-
 add5(number)
-
 
 
 function makeInt(){
   parseInt(number)
+  return number
+  
 }
 
 function preserveDecimal(){
   parseFloat(number)
+  return number
 }
 
 makeInt()
