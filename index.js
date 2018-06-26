b7908f6d2608df5b0eb4cc86f96c60cd2d3c818b
Anil Balram <adbalramd1@gmail.com>
2018-06-26 20:39:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,25 +6,31 @@ function add(){
   return numberZ
 }
 
-function divideBy3(){
-  return number
+function divideBy(){
+  var numberA
+  var numberB
+  var numberC
+  A/B
+  return numberC
 }
 
-divideBy3(15)
+divideBy(15,3)
 
 add(5,10)
 
 
 function makeInt(){
-  parseInt(number)
-  return number
+  var D
+  parseInt()
+  return D
 }
 
 function preserveDecimal(){
-  parseFloat(number)
-  return number
+  var I
+  parseFloat()
+  return I
 }
 
-makeInt(number)
+makeInt(8.123)
 
-preserveDecimal(number)
+preserveDecimal(8)
