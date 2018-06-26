dc2222db7ec058288ec9777d8f680367aaedc54d
Anil Balram <adbalramd1@gmail.com>
2018-06-26 20:22:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-var number = 5
-number++
-
-number--
-
-number += 3
-
-number -= 2
-
-number *= 10
-
-number /= 5
-
 var number = 10
+
 function add5(){
   number += 5
 }
@@ -26,13 +14,6 @@ console.log(number)
 add5(number)
 console.log(number)
 
-parseInt('2', 10)
-
-parseInt('2.2222', 10)
-
-parseInt('nonsense!', 10)
-
-parseFloat('80.123999')
 
 function makeInt(){
   parseInt(number)
@@ -42,6 +23,6 @@ function preserveDecimal(){
   parseFloat(number)
 }
 
-parseInt()
+makeInt()
 
-parseFloat()
+preserveDecimal()
